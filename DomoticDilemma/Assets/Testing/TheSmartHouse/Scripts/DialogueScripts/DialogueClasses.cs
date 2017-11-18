4d9923c3bda6e9f3ca8cb248d6ab9ebb87358daf
SamScott <samscott497@gmail.com>
2017-11-18 17:06:54 -0500

    Complete Dialogue classes and start dialogue system.

---
+++ DomoticDilemma/Assets/Testing/TheSmartHouse/Scripts/DialogueScripts/DialogueClasses.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+
+public class DialogueLine
+{
+	private string line;
+	private string alternate;
+
+	public bool isDecision;
+	private int[] decisionValues = new int[2];
+
+	public DialogueLine(string _line)
+	{
+		line = _line;
+		isDecision = false;
+	}
+
+	public DialogueLine(string _line, string _alternate, int decision1Value, int decision2Value)
+	{
+		line = _line;
+		alternate = _alternate;
+		isDecision = true;
+		decisionValues[0] = decision1Value;
+		decisionValues[1] = decision2Value;
+	}
+
+	public string GetText() {
+		return line;
+	}
+
+	public string GetAlternateText() {
+		return alternate;
+	}
+
+	public int GetDecisionValue(int decisionIndex) {
+		return decisionValues[decisionIndex];
+	}
+}
+
+public class DialogueChunk
+{
+	//finish regex
+	static Regex dialogueLinesRegex = new Regex("\r\n\"");
+
+	public string dialogueName;
+	private DialogueLine[] lines;
+	private int lineAmount;
+	
+	public DialogueChunk(string allTextInFile) {
+		//load file stuff here
+		//regex for dialogue lines, decisions, and decision values
+	}
+
+	public bool CheckLineForDecision(int lineIndex) {
+		if (lines[lineIndex].isDecision)
+			return true;
+		else
+			return false;
+	}
+
+	public string GetLineText(int lineIndex) {
+		return lines[lineIndex].GetText();
+	}
+
+	public string GetDecisionText(int lineIndex, int decisionIndex) {
+		if (decisionIndex == 0)
+			return lines[lineIndex].GetText();
+		else
+			return lines[lineIndex].GetAlternateText();
+	}
+
+	public int GetDecisionValue(int lineIndex, int decisionIndex) {
+		return lines[lineIndex].GetDecisionValue(decisionIndex);
+	}
+
+}
---
+++ DomoticDilemma/Assets/Testing/TheSmartHouse/Scripts/DialogueScripts/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	//all dialogue chunks
+	private DialogueChunk[] allDialogues;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//finish dialogue system
+	private void LoadAllDialogue() {
+		TextAsset[] dialogueFiles = Resources.LoadAll("Dialogue") as TextAsset[];
+		allDialogues = new DialogueChunk[dialogueFiles.Length];
+
+		for (int i = 0; i < dialogueFiles.Length; i++) {
+			allDialogues[i] = new DialogueChunk(dialogueFiles[i].text);
+		}
+	}
+
+}
