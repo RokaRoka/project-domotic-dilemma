8201eb23c0125fd4731f0d42fa524d1b420355eb
SamScott <samscott497@gmail.com>
2017-12-05 00:52:53 -0500

    Changes to EmailSystem for the future.

--- DomoticDilemma/Assets/EmailSystem/SwitchEmail.cs
+++ DomoticDilemma/Assets/EmailSystem/SwitchEmail.cs
@@ -5,28 +5,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class EmailEventArgs : EventArgs
+{
+    //Email index should be sent
+}
+
 public class SwitchEmail : MonoBehaviour {
 
     public delegate void ReadEmailEventHandler(object source, EventArgs e);
 
     public ReadEmailEventHandler ReadEmail;
 
-    public Text Email;
+    public Text EmailBody;
+    public Text EmailSubject;
+
+    private string[] emailBodyStrings;
+    private string[] emailSubjectStrings;
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
     public void Email1() {
-        Email.GetComponent<Text>().text = "Email1";
+        EmailBody.GetComponent<Text>().text = "Email1";
     }
     public void Email2()
     {
-        Email.GetComponent<Text>().text = "Email2";
+        EmailBody.GetComponent<Text>().text = "Email2";
     }
     public void Email3()
     {
-        Email.GetComponent<Text>().text = "Email3";
+        EmailBody.GetComponent<Text>().text = "Email3";
     }
 
     protected virtual void OnEmailRead()
