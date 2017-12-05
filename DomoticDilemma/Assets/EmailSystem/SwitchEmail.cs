1394387eaaf056f60d79498309dc984ec213f51e
NickMagnus <nickjmagnus@gmail.com>
2017-12-05 00:37:40 -0500

    Add Joe's Email System with start on delegate.

---
+++ DomoticDilemma/Assets/EmailSystem/SwitchEmail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchEmail : MonoBehaviour {
+
+    public delegate void ReadEmailEventHandler(object source, EventArgs e);
+
+    public ReadEmailEventHandler ReadEmail;
+
+    public Text Email;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Email1() {
+        Email.GetComponent<Text>().text = "Email1";
+    }
+    public void Email2()
+    {
+        Email.GetComponent<Text>().text = "Email2";
+    }
+    public void Email3()
+    {
+        Email.GetComponent<Text>().text = "Email3";
+    }
+
+    protected virtual void OnEmailRead()
+    {
+        if (ReadEmail != null)
+            ReadEmail(this, EventArgs.Empty);
+    }
+}
