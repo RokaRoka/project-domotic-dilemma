14d88a378830b87f95e398206d51857a13b914eb
Jonathan Nickerson <jnickersonnh@gmail.com>
2017-12-06 18:17:59 -0500

    Add Prefabs and fix water.

---
+++ DomoticDilemma/Assets/DialogueStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueStart : MonoBehaviour {
+
+	private DialogueManager dialogueManage;
+
+	public DialogueChunkName dialogueToLoad;
+
+	// Use this for initialization
+	void Start () {
+		dialogueManage = GameObject.FindGameObjectWithTag("DialogueController").GetComponent<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player")) {
+			if (!dialogueManage.CheckDialogueChunkComplete(dialogueToLoad))
+				dialogueManage.PlayDialogueChunk(dialogueToLoad);
+		}
+	}
+}
